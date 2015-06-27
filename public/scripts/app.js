2abcc3915fa90ac2386a04fdac1e81efbe4e40da
Brad Metcalf <brad@localabstract.com>
2015-06-27 15:04:14 -0500

    Isolated iframe and sandbox objects by name

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,51 +1,51 @@
 // All methods for your app you want protected go here
 var app = {
-  
-  copyAPI: function() {
-    var iframe = document.getElementById("canvas-iframe").contentWindow;
-
-    // Copy api object to the iframe
-    iframe.api = api;
-
-    // Convert api.move() to move() etc
-    for (key in api) {
-      iframe[key] = api[key];
-    }
-
-    // Prevent access to parent objects and their methods
-    iframe.api = "";
-    iframe.window.parent = "";
-  },
-
-  injectJS: function() {
-    var iframe = document.getElementById("canvas-iframe").contentWindow;
-
-    // Execution of move() will work
-    iframe.move();
-
-    // Execution of privateMethod() will work
-    iframe.privateMethod();
-
-    // Execution of app.privateMethod will fail
-    iframe.app.privateMethod();
-    
-    // Execution of app.privateMethod from the parent global will fail
-    iframe.parent.app.privateMethod();
-  },
 
   startApp: function() {
     var iframe = document.getElementById('canvas-iframe');
 
-    // Copy API to iframe and remove parent access
-    app.copyAPI();
+    // Copy API to sandbox and remove parent access
+    app.copyAPI(iframe);
 
     // Simulation of injected js
-    app.injectJS();
+    app.injectJS(iframe);
 
     // Recopy API and remove parent in the event user refreshes
     iframe.addEventListener('onload', function() {  
-      app.copyAPI();
+      app.copyAPI(iframe);
     });
+  },
+
+  copyAPI: function(iframe) {
+    var sandbox = iframe.contentWindow;
+
+    // Copy api object to the sandbox
+    sandbox.api = api;
+
+    // Convert api.move() to move() etc
+    for (key in api) {
+      sandbox[key] = api[key];
+    }
+
+    // Prevent access to parent objects and their methods
+    sandbox.api = "";
+    sandbox.window.parent = "";
+  },
+
+  injectJS: function(iframe) {
+    var sandbox = iframe.contentWindow;
+
+    // Execution of move() will work
+    sandbox.move();
+
+    // Execution of privateMethod() will work
+    sandbox.privateMethod();
+
+    // Execution of app.privateMethod will fail
+    sandbox.app.privateMethod();
+    
+    // Execution of app.privateMethod from the parent global will fail
+    sandbox.parent.app.privateMethod();
   },
 
   privateMethod: function() {
